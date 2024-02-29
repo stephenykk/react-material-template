ed2bfcee2e277bdfddb0061f894946d8c42c92c9
NEWAIM01\jack.pan <jack.pan@newaim.com.au>
2024-02-29 11:16:23 +0800

    feat: 修复components下组件代码丢失

--- src/components/SvgIcon.tsx
+++ src/components/SvgIcon.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
-
-function SvgIcon() {
-  return (
-    <div>
-      SvgIcon
-    </div>
-  );
+export interface SvgIconProps {
+  name: string
+  color?: string
+  stroke?: string
+  className?: string
+  size?: number | string
+  width?: number
+  height?: number
 }
 
-export default SvgIcon;
+const SvgIcon = (props: SvgIconProps) => {
+  const {
+    name,
+    color,
+    stroke = 'none',
+    size = 30,
+    width,
+    height,
+    className = '',
+  } = props
+
+  return (
+    <svg
+      aria-hidden="true"
+      width={width || size}
+      height={height || size}
+      fill={color}
+      className={'icon ' + `icon-${name} ` + className}
+    >
+      <use href={'#icon-' + name} fill={color} stroke={stroke} />
+    </svg>
+  )
+}
+export default SvgIcon
--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,11 +1,92 @@
-import React from 'react';
+import { forwardRef, useEffect, useState } from 'react'
+import ReactDOM from 'react-dom/client'
+import classnames from 'classnames'
+import styles from './Toast.module.scss'
+import SvgIcon from './SvgIcon'
 
-function Toast() {
-  return (
-    <div>
-      Toast
-    </div>
-  );
+export type ToastProps = {
+  delaySeconds?: number
+  msg: any
+  type?: 'warn' | 'success' | 'error'
+  onClose?: Fn
 }
 
-export default Toast;
+export const Toast = forwardRef<HTMLDivElement, ToastProps>(
+  (props: ToastProps, ref) => {
+    const { type = 'success', delaySeconds = 2 } = props
+    const [isEnter, setEnter] = useState(false)
+
+    const { onClose } = props
+
+    useEffect(() => {
+      setTimeout(() => setEnter(true))
+
+      setTimeout(() => {
+        onClose?.()
+        setEnter(false)
+      }, delaySeconds * 1000)
+    }, [onClose, delaySeconds])
+
+    const toastClass = classnames({
+      [styles.toast]: true,
+      [styles[`toast-${type}`]]: true,
+    })
+
+    const conClass = classnames({
+      [styles['toast-con']]: true,
+      [styles.enter]: isEnter,
+    })
+
+    return (
+      props.msg && (
+        <div ref={ref} className={toastClass} data-type={type}>
+          <div className={conClass}>
+            <span className="icon-wrap">
+              {type === 'success' ? (
+                <SvgIcon name="success" />
+              ) : (
+                <SvgIcon name="info" className={'icon-' + type} />
+              )}
+            </span>
+            {props.msg}
+          </div>
+        </div>
+      )
+    )
+  },
+)
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const showToast = (function getShowToast() {
+  let isShowing = false
+  const queue = [] as Array<ToastProps>
+
+  return function openToast(options: ToastProps) {
+    if (isShowing) {
+      queue.push(options)
+      return
+    }
+
+    let div = document.createElement('div')
+    div.className = 'toast-wrapper'
+    document.body.append(div)
+    const toastRoot = ReactDOM.createRoot(div)
+
+    const onClose = () => {
+      toastRoot.unmount()
+      document.body.removeChild(div)
+      div = null as any
+      isShowing = false
+
+      options.onClose?.()
+
+      // only one toast
+      if (queue.length) {
+        openToast(queue.shift()!)
+      }
+    }
+
+    toastRoot.render(<Toast {...options} onClose={onClose} />)
+    isShowing = true
+  }
+})()
